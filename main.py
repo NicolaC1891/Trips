7cf425c2b53db43d94426f3bc1edd4b93bc86b10
NicolaC <nicolassummoner@mail.ru>
2025-07-03 12:10:19 +0300

    Patch: Session closing

--- main.py
+++ main.py
@@ -22,14 +22,17 @@ async def main():
     Launches the bot, connects the dispatcher with startup and shutdown actions, connects routers, starts polling.
     :return:
     """
-    bot = Bot(token=config.BOT_TOKEN.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await bot.set_my_commands(commands=commands)
-    dp = Dispatcher()
-    dp.startup.register(startup)
-    dp.shutdown.register(shutdown)
-    dp.include_routers(router_menu, router_bel, router_rus)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    async with Bot(
+            token=config.BOT_TOKEN.get_secret_value(),
+            default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    ) as bot:
+        await bot.set_my_commands(commands=commands)
+        dp = Dispatcher()
+        dp.startup.register(startup)
+        dp.shutdown.register(shutdown)
+        dp.include_routers(router_menu, router_bel, router_rus)
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
 
 
 async def startup():
@@ -43,7 +46,7 @@ async def shutdown():
 if __name__ == "__main__":
     try:
         asyncio.run(main())
-    except AiogramError:
-        print("Unknown error")
     except KeyboardInterrupt:
         print("App terminated")
+    except Exception as e:
+        print("Unknown error:", type(e).__name__, "-", e)
